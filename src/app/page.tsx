36ec3b670f1e3058125c69aa6f38ebcf684790ff
vaguiiinho <vaguiiinho5@gmail.com>
2024-11-14 17:26:40 -0300

    Estilado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { FaDownload } from "react-icons/fa";
+
 export default async function Page() {
   const url = 'url';
   const token = 'token';
@@ -10,7 +12,7 @@ export default async function Page() {
     },
     body: JSON.stringify({
       qtype: "fn_areceber.id_cliente",
-      query: "id"
+      query: "14149"
     }),
   };
 
@@ -25,10 +27,33 @@ export default async function Page() {
   let faturas = await data.json();
 
   return (
-    <main>
+    <main className="flex flex-col items-center mt-10">
       {faturas.registros.map((fatura: any) => (
-        <ul key={fatura.id}>
-          <li>id: {fatura.id} valor: {fatura.valor}</li>
+        <ul key={fatura.id} className="w-full max-w-md mb-4 border border-gray-200 rounded-lg shadow">
+          <li className="p-4 bg-white flex flex-col space-y-2 rounded-md">
+            <div className="flex items-center justify-between space-x-4">
+              <span className="text-lg font-semibold">ID: {fatura.id}</span>
+              <span className="text-gray-700">Valor: {fatura.valor}</span>
+              <span
+                className={`py-1 px-3 rounded-full text-sm font-medium 
+                  ${fatura.status === "R" ? "bg-green-100 text-green-700" : 
+                  fatura.status === "C" ? "bg-red-100 text-red-700" : 
+                  "bg-yellow-100 text-yellow-700"}`}
+              >
+                Status: {fatura.status === "R" ? "Recebido" : fatura.status === "C" ? "Cancelado" : "Aberto"}
+              </span>
+            </div>
+
+            <div className="flex space-x-4 mt-3">
+              <button className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:outline-none">
+                Visualizar
+              </button>
+              <button className="px-4 py-2 bg-gray-200 text-gray-700 rounded-lg hover:bg-gray-300 focus:outline-none flex items-center space-x-1">
+                <FaDownload /> {/* Ícone de download */}
+                <span>Download</span>
+              </button>
+            </div>
+          </li>
         </ul>
       ))}
     </main>
